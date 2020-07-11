5fc9ba5193bda148847f9b4265636064202bd323
RujH <rujhaan01@gmail.com>
2020-07-10 19:20:58 -0500

    closet page buttons done

--- lib/screens/pages/Closet/closet.dart
+++ lib/screens/pages/Closet/closet.dart
@@ -1,3 +1,5 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/services/auth.dart';
 import 'package:myapp/services/database.dart';
@@ -11,87 +13,125 @@ class Closet extends StatelessWidget {
   Widget build(BuildContext context) {
       return Scaffold(
         backgroundColor: Colors.white,
-        body: new Column(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Align(
               alignment: Alignment.center,
               child: Padding(
-                padding: EdgeInsets.only(top: 50.0, bottom: 20.0),
-              ),
+                  padding: EdgeInsets.only(top: 50.0, bottom: 20.0),
+                  ),
             ),
-            Container(
+            InkWell(
+                onTap: () {print("box 1 ");},
+                child: Container(
+                      width: 380.0,
+                      height: 150.0,
+                      padding: EdgeInsets.only(top: 40.0),
+                      child: Text(
+                        "Clothing",
+                        textAlign: TextAlign.center,
+                        style: TextStyle( color: Colors.white,  fontWeight: FontWeight.bold, fontSize: 50, fontStyle:FontStyle.normal ),
+
+                      ),
+                      decoration: BoxDecoration(
+
+                          color: Color(0xffEA9F5A),
+                          borderRadius: BorderRadius.circular(20.0),
+                          border: Border.all(
+                            color: Colors.grey.withOpacity(0.5), width: .2
+                          ),
+                    ),
+                ),
+              ),
+
+
+          Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.only( bottom: 20.0),
+              ),
+          ),
+          InkWell(
+            onTap: () {print("box 2 ");},
+            child: Container(
               width: 380.0,
               height: 150.0,
+              padding: EdgeInsets.only(top: 40.0),
+              child: Text(
+                "Shoes",
+                textAlign: TextAlign.center,
+                style: TextStyle( color: Colors.white,  fontWeight: FontWeight.bold, fontSize: 50, fontStyle:FontStyle.normal ),
+
+              ),
               decoration: BoxDecoration(
-                color: Color(0xff2F3641),
+                color: Color(0xffEA9F5A),
                 borderRadius: BorderRadius.circular(20.0),
                 border: Border.all(
                   color: Colors.grey.withOpacity(0.3), width: .2
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: EdgeInsets.only( bottom: 20.0),
-              ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
             ),
-            Container(
+          ),
+
+          InkWell(
+            onTap: () {print("box 3 ");},
+            child: Container(
               width: 380.0,
               height: 150.0,
+              padding: EdgeInsets.only(top: 40.0),
+              child: Text(
+                "Accessories",
+                textAlign: TextAlign.center,
+                style: TextStyle( color: Colors.white,  fontWeight: FontWeight.bold, fontSize: 50, fontStyle:FontStyle.normal ),
+
+              ),
               decoration: BoxDecoration(
-                color: Color(0xffFF7F50),
-                borderRadius: BorderRadius.circular(20.0),
-                border: Border.all(
-                    color: Colors.grey.withOpacity(0.3), width: .2
-                ),
+                  color: Color(0xffEA9F5A),
+                  borderRadius: BorderRadius.circular(20.0),
+                  border: Border.all(
+                      color: Colors.grey.withOpacity(0.3), width: .2
+                  ),
               ),
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 20.0),
-              ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
             ),
-            Container(
+          ),
+         InkWell(
+          onTap: () {print("box 4 ");},
+          child: Container(
               width: 380.0,
               height: 150.0,
+            padding: EdgeInsets.only(top: 40.0),
+            child: Text(
+              "Wish List",
+              textAlign: TextAlign.center,
+              style: TextStyle( color: Colors.white,  fontWeight: FontWeight.bold, fontSize: 50, fontStyle:FontStyle.normal ),
+
+            ),
               decoration: BoxDecoration(
-                color: Color(0xff2F3641),
-                borderRadius: BorderRadius.circular(20.0),
-                border: Border.all(
+                  color: Color(0xffEA9F5A),
+                  borderRadius: BorderRadius.circular(20.0),
+                  border: Border.all(
                     color: Colors.grey.withOpacity(0.3), width: .2
-                ),
+                  ),
               ),
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 20.0),
-              ),
-            ),
-            Container(
-              width: 380.0,
-              height: 150.0,
-              decoration: BoxDecoration(
-                color: Color(0xffFF7F50),
-                borderRadius: BorderRadius.circular(20.0),
-                border: Border.all(
-                    color: Colors.grey.withOpacity(0.3), width: .2
-                ),
-              ),
-            ),
-          ],
+          ),
+         ],
         ),
       );
-//    return Container(
-//      color: Color(0xff2F3641),
-//      child: Center(
-//          child: Text (
-//            "Clothes page",
-//            style: TextStyle(fontSize: 20,  color: Colors.white, fontWeight: FontWeight.bold),
-//          )
-//      ),
-//    );
+
   }
 }
